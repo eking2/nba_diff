8cb86b39a3748620db6fad45e88971e2bb5d0dfc
eking2 <eking2@uci.edu>
2021-02-20 01:27:36 -0800

    plots working

--- main.py
+++ main.py
@@ -1,6 +1,73 @@
 import streamlit as st
 import datetime
 import nba_diff
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from nba_diff import game
+from typing import Dict, Tuple
+
+@st.cache
+def full_data(season: str, gameid: str) -> Tuple[game, np.ndarray, pd.DataFrame]:
+
+    '''parse all records'''
+
+    # play-by-play and game details
+    game_score = nba_diff.get_game_score(season, gameid)
+    pbp = nba_diff.get_game_data(season, gameid, 'pbp')
+    game_detail = nba_diff.get_game_data(season, gameid, 'gamedetail')
+
+    # setup player data to hold rotations
+    home_players = nba_diff.get_player_data(game_detail, 'home')
+    visiting_players = nba_diff.get_player_data(game_detail, 'visiting')
+    players = {**home_players, **visiting_players}
+
+    # differential from play-by-play
+    diff = nba_diff.get_differential(pbp)
+
+    # merge player rotation times over periods
+    players = nba_diff.get_rotations(pbp, players)
+    players = nba_diff.merge_rotations(players)
+
+    # to df and keep only ingame
+    rotations_df = nba_diff.players_to_df(players)
+    ingame_df = nba_diff.get_player_ingame(rotations_df)
+
+    return game_score, diff, ingame_df
+
+
+def game_played(gameid: str, scoreboard: pd.DataFrame) -> bool:
+
+    '''check no na, from postponed games'''
+
+    done = scoreboard.query("gameid == @gameid")['visiting_score'].values[0] != ''
+
+    return done
+
+
+def full_plot(ingame_df: pd.DataFrame, diff: np.ndarray, game_score: game, home_c: str, team_c: str) -> mpl.figure.Figure:
+
+    '''plot player rotations and differential'''
+
+    fig, ax = plt.subplots(3, 1, figsize=(10, 15))
+
+    nba_diff.plot_rotation(ingame_df, 'visiting', visit_c, ax=ax.flat[0])
+    nba_diff.plot_rotation(ingame_df, 'home', home_c, ax=ax.flat[2])
+
+    # need to manually set symmetric ylim, does not auto set with subplots
+    nba_diff.plot_differential(diff, game_score, home_c, visit_c, ax=ax.flat[1])
+    max_y = np.abs(diff).max(axis=0)[1] * 1.1
+    ax.flat[1].set_ylim([-max_y, max_y])
+
+    plt.tight_layout()
+
+    return fig
+
+
+###############################################################################
+
+color = pd.read_csv('colors.csv')
 
 st.header('NBA Game Differentials')
 
@@ -9,12 +76,14 @@ game_date = st.date_input('Select Date')
 
 year = game_date.year
 month = game_date.month
-date = game_date.day
+date = game_date.day 
 
 # show games and dropdown to select
+# start with no selection
 scoreboard = nba_diff.get_scoreboard(year, month, date)
 game_descs = [f'{gameid} ({vis} vs. {home})' for gameid, vis, home in 
                 zip(scoreboard['gameid'], scoreboard['visiting'], scoreboard['home'])]
+game_descs.insert(0, None)
 
 if len(scoreboard) == 0:
     st.write(f'No games for {year}-{month}-{date}')
@@ -22,10 +91,26 @@ else:
     st.write(scoreboard[['gameid', 'visiting', 'home', 'visiting_score', 'home_score']])
     game_desc = st.selectbox('Select Game', game_descs)
 
-    # gameid and seasonyear for game data
-    gameid = game_desc.split(' ')[0]
-    seasonyear = scoreboard.query("gameid == @gameid")['seasonYear'].values[0]
+    if game_desc is not None:
 
+        # gameid and seasonyear for game data
+        gameid = game_desc.split(' ')[0]
+        seasonyear = scoreboard.query("gameid == @gameid")['seasonYear'].values[0]
+        vis = scoreboard.query("gameid == @gameid")['visiting'].values[0]
+        home = scoreboard.query("gameid == @gameid")['home'].values[0]
 
-    print(gameid)
-    print(seasonyear)
+        # plot vaild games
+        if game_played(gameid, scoreboard):
+            game_score, diff, ingame_df = full_data(seasonyear, gameid)
+
+            # team colors
+            home_c = color.query("team == @home")['color'].values[0]
+            visit_c = color.query("team == @vis")['color'].values[0]
+
+            fig = full_plot(ingame_df, diff, game_score, home_c, visit_c)
+            st.write(fig)
+            
+
+        else:
+            st.write(f'{gameid} ({vis} vs. {home}) has not been played')
+
--- nba_diff.py
+++ nba_diff.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from dataclasses import dataclass
 from copy import deepcopy
-from typing import Optional
+from typing import Optional, Dict
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
@@ -18,7 +18,7 @@ class game:
     visiting: str
     home_score: int
     visiting_score: int
-    
+
 
 @dataclass
 class rotation:
@@ -27,8 +27,8 @@ class rotation:
     start_time: float
     # negative values when player never substituted out
     end_time: float = -1
-        
-        
+
+
 @dataclass
 class player:
     '''player info'''
@@ -40,55 +40,55 @@ class player:
 ###############################################################################
 # parse data
 
-def get_game_data(season: str, gameid: str, data_type: str) -> dict:
-    
+def get_game_data(season: str, gameid: str, data_type: str) -> Dict:
+
     '''download play by play or game detail json data'''
-    
+
     assert data_type in ['pbp', 'gamedetail'], 'invalid game data type'
-    
+
     if data_type == 'pbp':
         url = f'http://data.nba.net/v2015/json/mobile_teams/nba/{season}/scores/pbp/{gameid}_full_pbp.json'
     elif data_type == 'gamedetail':
         url = f'http://data.nba.net/v2015/json/mobile_teams/nba/{season}/scores/gamedetail/{gameid}_gamedetail.json'
-        
+
     r = requests.get(url)
     assert r.status_code == 200, f'invalid {data_type} request for season: {season}, gameid: {gameid}'
 
-    content = json.loads(r.text) 
-    
+    content = json.loads(r.text)
+
     return content
 
 
 def get_game_score(season: str, gameid: str) -> game:
-    
+
     '''get final score and team info'''
-    
+
     content = get_game_data(season, gameid, 'gamedetail')
-    
+
     # 3-letter team abbrev
     home = content['g']['hls']['ta']
     visiting = content['g']['vls']['ta']
-    
+
     # points scored
     home_score = content['g']['hls']['s']
     visiting_score = content['g']['vls']['s']
-    
+
     game_data = game(gameid, home, visiting, home_score, visiting_score)
-    
+
     return game_data
 
 
-def get_player_data(game_detail: dict, team: str) -> dict:
-    
+def get_player_data(game_detail: dict, team: str) -> Dict:
+
     '''dict of player id and info from game details. use to hold rotations'''
 
     assert team in ['home', 'visiting'], "invalid team option, choose ['home', 'visiting']"
-    
+
     if team == 'home':
         roster = game_detail['g']['hls']['pstsg']
     else:
         roster = game_detail['g']['vls']['pstsg']
-        
+
     players = {}
     for p in roster:
         if p['totsec'] > 0:
@@ -98,44 +98,44 @@ def get_player_data(game_detail: dict, team: str) -> dict:
             rot = rotation(0, 0)
             p_data = player(fn, ln, team, [rot])
             players[player_id] = p_data
-        
+
     return players
 
 
 def get_differential(pbp: dict) -> np.ndarray:
-    
+
     '''record point differential between home and visiting team over time'''
-    
+
     res = []
-    
+
     # hs - vs (home - visiting)
     for period in pbp['g']['pd']:
         for play in period['pla']:
             if play['etype'] == 1 or play['etype'] == 3:
-                
+
                 # store time counting up from beginning of game and differential
                 time = cl_to_seconds(play['cl'], period['p'])
                 hs = play['hs']
                 vs = play['vs']
                 diff = hs - vs
-                
+
                 res.append([time, diff])
-                
+
     # [time(s), point differential]
     # + is for home, - is for visiting
     return np.array(res)
 
 
-def get_rotations(pbp: dict, players: dict) -> dict:
+def get_rotations(pbp: dict, players: dict) -> Dict:
 
     '''parse play-by-play for player substitution times'''
 
     players_copy = deepcopy(players)
-    
+
     # data stored per period
     for period in pbp['g']['pd']:
         for event in period['pla']:
-            
+
             # 8 = substitution
             if event['etype'] == 8:
                 player_in = int(event['epid'])
@@ -179,18 +179,18 @@ def get_rotations(pbp: dict, players: dict) -> dict:
                         players_copy[assister].rotations[-1].ingame = 1
                     except:
                         continue
-                        
+
     return players_copy
 
 
 def get_player_ingame(rotation_df: pd.DataFrame) -> pd.DataFrame:
 
     '''time windows for when each player is in game'''
-    
+
     # need time span for broken hbar, plots x + xwidth
-    ingame_df = rotation_df.query("ingame == 1") 
+    ingame_df = rotation_df.query("ingame == 1")
     ingame_df = ingame_df.assign(xwidth = lambda x: x['end'] - x['start'])
-    
+
     player_times = []
     for player_id in rotation_df['id'].unique():
         subset = ingame_df.query("id == @player_id")
@@ -198,28 +198,28 @@ def get_player_ingame(rotation_df: pd.DataFrame) -> pd.DataFrame:
         name = ' '.join(list(map(str, name)))
         times = subset[['start', 'xwidth']].values.tolist()
         team = subset['team'].tolist()[0]
-        
+
         player_times.append([player_id, name, team, times])
-        
+
     df = pd.DataFrame(player_times, columns=['id', 'name', 'team', 'times'])
-    
+
     return df
 
 
 @st.cache
 def get_scoreboard(year: str, month: str, date: str) -> pd.DataFrame:
-    
+
     '''download nba boxscore data for games on input date'''
-    
-    # pad zeros 
+
+    # pad zeros
     url = f'https://data.nba.net/data/10s/prod/v1/{year:0>4}{month:0>2}{date:0>2}/scoreboard.json'
-    
+
     r = requests.get(url)
     assert r.status_code == 200, 'invalid request'
-    
+
     # get stats json
     scoreboard = json.loads(r.text)
-    
+
     # to dataframe
     res = []
     num_games = scoreboard['numGames']
@@ -228,12 +228,12 @@ def get_scoreboard(year: str, month: str, date: str) -> pd.DataFrame:
         visit = scoreboard['games'][i]['vTeam']['triCode']
         home = scoreboard['games'][i]['hTeam']['triCode']
         season_year = scoreboard['games'][i]['seasonYear']
-        
+
         visit_score = scoreboard['games'][i]['vTeam']['score']
         home_score = scoreboard['games'][i]['hTeam']['score']
-        
+
         res.append([game_id, season_year, visit, home, visit_score, home_score])
-    
+
     df = pd.DataFrame(res, columns=['gameid', 'seasonYear', 'visiting', 'home', 'visiting_score', 'home_score'])
 
     return df
@@ -243,78 +243,78 @@ def get_scoreboard(year: str, month: str, date: str) -> pd.DataFrame:
 # utils
 
 def cl_to_seconds(cl: str, period: int) -> float:
-    
+
     '''convert cl timing to game time elapsed in seconds'''
-    
+
     # cl counts down from 12 min per period
     # DOES NOT ACCOUNT FOR OT
-    
+
     mins = int(cl.split(':')[0])
     secs = float(cl.split(':')[1])  # time has decimals below 1 min
-    
+
     # cl resets every period
     p = period * 12 * 60
-    
+
     # count down
     elapsed = p - (mins*60) - secs
-    
+
     # in seconds
     return elapsed
 
 
 def players_to_df(players: dict) -> pd.DataFrame:
-    
+
     '''convert players data from dict to dataframe'''
-    
+
     to_df = []
     for player_id, player in players.items():
         first_name = player.first_name
         last_name = player.last_name
         team = player.team
-        
+
         for rotation in player.rotations:
             ingame = rotation.ingame
             start = rotation.start_time
             end = rotation.end_time
-            
+
             to_df.append([first_name, last_name, player_id, team, ingame, start, end])
-            
+
     df = pd.DataFrame(to_df, columns=['fn', 'ln', 'id', 'team', 'ingame', 'start', 'end'])
-    
+
     return df
 
 
-def merge_rotations(players: dict) -> dict:
+def merge_rotations(players: dict) -> Dict:
 
     '''merge timing that spans period breaks, players will have consecutive rotation records with ingame=1'''
 
     players_copy = deepcopy(players)
-    
+
     for player_id, player in players_copy.items():
         i = 0
         while i < len(player.rotations) - 1:
             # ingame = 1 through quarters
             if player.rotations[i].ingame == player.rotations[i+1].ingame:
-                
+
                 # extend end_time for rotation
                 player.rotations[i].end_time = player.rotations[i+1].end_time
-                
+
                 # remove next rotation, no longer necessary since prior rotation extended
                 player.rotations.pop(i+1)
 
             else:
                 i += 1
-    
-    return players_copy 
+
+    return players_copy
 
 
 ###############################################################################
 # plotting
 
-def plot_differential(diff: np.ndarray, 
-                      game_score: game, 
-                      home_color: str, 
-                      visit_color: str, 
+def plot_differential(diff: np.ndarray,
+                      game_score: game,
+                      home_color: str,
+                      visit_color: str,
                       ax: Optional[mpl.axes.SubplotBase] = None) -> None:
 
     '''plot point differentials'''
@@ -334,52 +334,51 @@ def plot_differential(diff: np.ndarray,
     ax.axhline(0, color='k', alpha=0.5)
 
     # fill steps with different colors
-    ax.fill_between(diff[:, 0], diff[:, 1], step='mid', alpha=0.3, where=diff[:, 1] >= 0, 
+    ax.fill_between(diff[:, 0], diff[:, 1], step='mid', alpha=0.3, where=diff[:, 1] >= 0,
                     facecolor=home_color, interpolate=True, label=game_score.home)
-    ax.fill_between(diff[:, 0], diff[:, 1], step='mid', alpha=0.3, where=diff[:, 1] <= 0, 
+    ax.fill_between(diff[:, 0], diff[:, 1], step='mid', alpha=0.3, where=diff[:, 1] <= 0,
                     facecolor=visit_color, interpolate=True, label=game_score.visiting)
 
     ax.grid(alpha=0.2)
     ax.set_axisbelow(True)
     ax.set_ylabel('Differential')
     ax.legend()
-    
-    ax.set_title(f'{game_score.home} vs. {game_score.visiting}\n{game_score.home_score} - {game_score.visiting_score}', size=13)
-    
+
+    ax.set_title(f'{game_score.visiting} vs. {game_score.home}\n{game_score.visiting_score} - {game_score.home_score}', size=15)
+
     # set symmetic ylim
     y_max = max(list(map(abs, plt.gca().get_ylim())))
     ax.set_ylim([-y_max, y_max])
 
 
-def plot_rotation(ingame_df: pd.DataFrame, 
-                  team: str, 
-                  color: str, 
+def plot_rotation(ingame_df: pd.DataFrame,
+                  team: str,
+                  color: str,
                   ax: Optional[mpl.axes.SubplotBase] = None) -> None:
-    
+
     '''plot rotation chart'''
-    
+
     if ax is None:
         ax = plt.gca()
-    
+
     subset = ingame_df.query("team == @team")
-    
-    # idx is the df index
+
     for i, (idx, row) in enumerate(subset.iterrows()):
         times = row['times']
         ax.broken_barh(times, (i-0.25, 0.5), color=color, edgecolor='k', alpha=0.5)
-        
+
     # split into periods
     x_la = ['Q1', 'Q2', 'Q3', 'Q4']
     x_ra = [i*12*60 for i in range(len(x_la))]
     ax.set_xticks(x_ra)
     ax.set_xticklabels(x_la)
-    
+
     # y label player names
     y_la = subset['name'].values
     y_ra = np.arange(len(y_la))
     ax.set_yticks(y_ra)
     ax.set_yticklabels(y_la, size=12)
-    
+
     ax.invert_yaxis()
     ax.grid(alpha=0.2)
     ax.set_axisbelow(True)
@@ -387,8 +386,9 @@ def plot_rotation(ingame_df: pd.DataFrame,
 
 ###############################################################################
 
-season = '2019'
-gameid = '0041900401'
+
+#season = '2019'
+#gameid = '0041900401'
 
 #df = get_scoreboard('2020', '9', '8')
 #print(df)
